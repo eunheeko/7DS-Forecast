654891ffebb02f693b9ad8103361ca4d2e6498ab
eunheeko <geh1234@snu.ac.kr>
2023-09-01 11:13:03 +0900

    added changes in machine.;y

--- machine.py
+++ machine.py
@@ -1,17 +1,18 @@
-from svnds.synphot import Syn7DS, merge_tbl
+from svnds.synphot import Syn7DS, SynSPx, merge_tbl
 import numpy as np
 import os
+from astropy.table import Table
 
-mag_ref = 23
-path_elcosmos = '/data8/EL_COSMOS/elcosmos/'
-path_save = f'/data8/EL_COSMOS/synphots/SDS/scale_{mag_ref:d}_eff/'
+for mag_ref in [20.5, 21.5, 22.5]:
 
-Tsamps = np.array([180, 180 * 364/14, 180 * 364/14 * 3, 180 * 364/14 * 5, 180 * 364/14 * 7, 180 * 365, 180 * 365 * 5], dtype = int)
-Tsamps = (Tsamps * 0.5).astype(int)
+    path_elcosmos = '/data8/EL_COSMOS/elcosmos/'
+    # path_save = f'/data8/EL_COSMOS/synphots/SDS_m6250/scale_{mag_ref:.1f}_eff/'
+    path_save = f'/data8/EL_COSMOS/synphots/SPHEREx_m6250/scale_{mag_ref:.1f}/'
 
-syn = Syn7DS(path_elcosmos, path_save, Tsamps)
+    # Tsamps = np.array([180 * 364/14 * 5])
+    # Tsamps = (Tsamps * 0.5).astype(int)
+    syn = SynSPx(path_elcosmos, path_save)
 
-syn.synphot(zones = range(1, 10), magscale = True, mag_ref = mag_ref)
+    syn.synphot(zones = range(1, 10), magscale = True, mag_ref = mag_ref)
 
-merge_tbl(path_save, f"synphot_7DS_scale_{mag_ref:d}_yrs_all_eff_tot.csv")
-
+    merge_tbl(path_save, f"synphot_SPHEREx_scale_{mag_ref:.1f}_tot.csv")
