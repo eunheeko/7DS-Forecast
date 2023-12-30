ec280c0c93e179f71b70377417f32b397dd257bb
eunheeko <eunhee.ko.astro@gmail.com>
2023-12-30 17:19:59 +0900

    edited setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ from os.path import basename, splitext
 from setuptools import setup, find_packages
 from svnds import __version__
 
+
 version = __version__
 setup(
     name = 'svnds',
     version = version,
     author = 'Eunhee Ko',
-    packages = find_packages(),
     package_data = {'': ['data/*']},
     include_package_data = True,
     py_modules = [splitext(basename(path))[0] for path in glob('svnds/*.py')]
